27fdd32a214792d44c3b3f82fa99c64ed1703283
Andrew Myers <andrew_myers@wgbh.org>
2013-05-26 17:46:54 -0400

    Added module Artesia::Datastreams::UOIS, containing a class method for adding UOIS terminology to datastreams.

--- app/models/datastream/teams_asset_file.rb
+++ app/models/datastream/teams_asset_file.rb
@@ -1,99 +1,28 @@
-class Datastream::TeamsAssetFile < ActiveFedora::OmDatastream
-  set_terminology do |t|
+require 'artesia'
 
+class Datastream::TeamsAssetFile < ActiveFedora::OmDatastream
+
+  include Artesia::Datastreams::UOIS
+
+  set_terminology do |t|
 
     t.root(:path => "TEAMS_ASSET_FILE")
 
     t.assets(:path => "ASSETS") {
       t.asset(:path => "ASSET") {
         t.metadata(:path => "METADATA") {
-
           t.uois(:path => "UOIS") {
-            t.version(:path => {:attribute => "VERSION"})
-            t.import_dt(:path => {:attribute => "IMPORT_DT"})
-            t.uoi_id(:path => {:attribute => "UOI_ID"})
-            t.name(:path => {:attribute => "NAME"})
-            t.import_user_id(:path => {:attribute => "IMPORT_USER_ID"})
-            t.import_id(:path => {:attribute => "IMPORT_ID"})
-          }
-
-          t.security_policy_uois(:path => "SECURITY_POLICY_UOIS") {
-            t.sec_policy_id(:path => {:attribute => "SEC_POLICY_ID"})
-          }
-
-          t.wgbh_rights(:path => "WGBH_RIGHTS") {
-            t.rights_note(:path => {:attribute => "RIGHTS_NOTE"})
-            t.rights_type(:path => {:attribute => "RIGHTS_TYPE"})
-            t.rights_credit(:path => {:attribute => "RIGHTS_CREDIT"})
-            t.rights_holder(:path => {:attribute => "RIGHTS_HOLDER"})
-          }
-
-          t.wgbh_subject(:path => "WGBH_SUBJECT") {
-            t.subject(:path => {:attribute => "SUBJECT"})
-          }
-
-          t.wgbh_type(:path => 'WGBH_TYPE') {
-            t.item_type(:path => {:attribute => 'ITEM_TYPE'})
-          }
-
-          t.wgbh_description(:path => "WGBH_DESCRIPTION") {
-            t.description_type(:path => {:attribute => "DESCRIPTION_TYPE"})
-            t.description_coverage_in(:path => {:attribute => "DESCRIPTION_COVERAGE_IN"})
-            t.description_coverage(:path => {:attribute => "DESCRIPTION_COVERAGE"})
-            t.description_coverage_out(:path => {:attribute => "DESCRIPTION_COVERAGE_OUT"})
-            t.description(:path => {:attribute => "DESCRIPTION"})
-          }
-
-          t.wgbh_format(:path => "WGBH_FORMAT") {
-            t.dimensions_height(:path => {:attribute => "DIMENSIONS_HEIGHT"})
-            t.aspect_ratio(:path => {:attribute => "ASPECT_RATIO"})
-            t.broadcast_format(:path => {:attribute => "BROADCAST_FORMAT"})
-            t.duration(:path => {:attribute => "DURATION"})
-            t.color(:path => {:attribute => "COLOR"})
-            t.dimension_width(:path => {:attribute => "DIMENSIONS_WIDTH"})
-            t.item_format(:path => {:attribute => "ITEM_FORMAT"})
-            t.mime_type(:path => {:attribute => "MIME_TYPE"})
-          }
-
-          t.wgbh_title(:path => "WGBH_TITLE") {
-            t.title_type(:path => {:attribute => "TITLE_TYPE"})
-            t.title(:path => {:attribute => "TITLE"})
-          }
-
-          t.wgbh_annotation(:path => "WGBH_ANNOTATION") {
-            t.annotation(:path => {:attribute => "ANNOTATION"})
-            t.annotation_type(:path => {:attribute => "ANNOTATION_TYPE"})
-          }
-
-          t.wgbh_source(:path => "WGBH_SOURCE") {
-            t.source_type(:path => {:attribute => "SOURCE_TYPE"})
-            t.source(:path => {:attribute => "SOURCE"})
-          }
-
-          t.wgbh_language(:path => "WGBH_LANGUAGE") {
-            t.language_usage(:path => {:attribute => "LANGUAGE_USAGE"})
-            t.language(:path => {:attribute => "LANGUAGE"})
-          }
-
-          t.wgbh_publisher(:path => "WGBH_PUBLISHER") {
-            t.publisher_type(:path => {:attribute => "PUBLISHER_TYPE"})
-            t.publisher(:path => {:attribute => "PUBLISHER"})
-          }
-
-          t.wgbh_holdings(:path => "WGBH_HOLDINGS") {
-            t.holdings_department(:path => {:attribute => "HOLDINGS_DEPARTMENT"})
+            add_uois_terminology t  
           }
         }
-
         t.content(:path => "CONTENT")
-        
       }
     }
   end
 
   class << self
     def xml_template
-      Nokogiri::XML.parse("<UOIS/>")
+      Nokogiri::XML.parse("<TEAMS_ASSET_FILE/>")
     end  
   end
   
