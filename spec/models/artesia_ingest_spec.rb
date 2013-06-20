8c3cbfd52173d8209a4496af1736ca63bd8e9fdb
Andrew Myers <andrew_myers@wgbh.org>
2013-06-20 19:36:47 -0400

    Adds tests for relationships between ArtesiaIngest and OpenvaultAsset models. Closes #58.

--- spec/models/artesia_ingest_spec.rb
+++ spec/models/artesia_ingest_spec.rb
@@ -9,6 +9,44 @@ describe ArtesiaIngest do
   let(:invalid_xml) { 'this is not xml' }
   let(:depositor) { 'openvault_testing@wgbh.org' }
 
+  it 'saves has_many relationships when unsaved OpenvaultAsset models are added to saved ArtesiaIngest' do
+    ov1, ov2 = OpenvaultAsset.new, OpenvaultAsset.new
+    ov1.apply_depositor_metadata depositor
+    ov2.apply_depositor_metadata depositor
+    artesia_ingest.save!
+    artesia_ingest.openvault_assets << ov1 << ov2
+    check1, check2 = OpenvaultAsset.find(ov1.pid), OpenvaultAsset.find(ov2.pid)
+    artesia_ingest.openvault_assets[0].should == check1
+    artesia_ingest.openvault_assets[1].should == check2
+  end
+
+  it 'saves has_many relationships when saved OpenvaultAsset models are added to unsaved ArtesiaIngest, and ArtesiaIngest is saved' do
+    ov1, ov2 = OpenvaultAsset.new, OpenvaultAsset.new
+    ov1.apply_depositor_metadata depositor
+    ov2.apply_depositor_metadata depositor
+    ov1.save!
+    ov2.save!
+    artesia_ingest.openvault_assets << ov1 << ov2
+    artesia_ingest.save!
+    check1, check2 = OpenvaultAsset.find(ov1.pid), OpenvaultAsset.find(ov2.pid)
+    artesia_ingest.openvault_assets.should == [check1, check2]
+  end
+
+  it 'saves has_many relationships when saved ArtesiaIngest is added to OpenvaultAsset and OpenvaultAsset is saved' do
+    ov1, ov2 = OpenvaultAsset.new, OpenvaultAsset.new
+    ov1.apply_depositor_metadata depositor
+    ov2.apply_depositor_metadata depositor
+    artesia_ingest.save!
+    ov1.artesia_ingest = artesia_ingest
+    ov2.artesia_ingest = artesia_ingest
+    ov1.save!
+    ov2.save!
+    check = ArtesiaIngest.find(artesia_ingest.pid)
+    check.openvault_assets.should == [ov1, ov2]
+  end
+
+  
+
   describe '#save_all!' do
     it 'saves 1 record for the ingest itself, and 1 record for each child asset' do
       count_before_ingest = ActiveFedora::Base.count
--- spec/models/openvault_asset_spec.rb
+++ spec/models/openvault_asset_spec.rb
@@ -3,14 +3,33 @@ require 'spec_helper'
 describe OpenvaultAsset do
 
   let(:valid_xml) { File.read("#{fixture_path}/artesia_ingest/uois.zoom_sample.xml") }
+  let(:depositor) { "openvault_testing@wgbh.org" }
 
   subject (:ov_asset) { OpenvaultAsset.new }
 
   it "saves a datastream for UOIS xml" do
     ov_asset.uois.set_xml valid_xml
-    ov_asset.apply_depositor_metadata "openvault_testing@wgbh.org"
+    ov_asset.apply_depositor_metadata depositor
     ov_asset.save!
     compare = ActiveFedora::Base.find ov_asset.pid, :cast => true
     compare.uois.to_xml.should == ov_asset.uois.to_xml
   end
+
+  it 'saves belongs_to relationship when unsaved OpenvaultAsset models are added to saved ArtesiaIngest' do
+    ov_asset.apply_depositor_metadata depositor
+    artesia_ingest = ArtesiaIngest.create!
+    artesia_ingest.openvault_assets << ov_asset
+    check = ActiveFedora::Base.find ov_asset.pid, :cast => true
+    check.artesia_ingest.should == artesia_ingest
+  end
+
+  it 'saves belongs_to relationship when saved ArtesiaIngest is added to OpenvaultAsset via #artesia_ingest, and OpenvaultAsset is saved' do
+    artesia_ingest = ArtesiaIngest.create!
+    ov_asset.apply_depositor_metadata depositor
+    ov_asset.artesia_ingest = artesia_ingest
+    ov_asset.save!
+    check = OpenvaultAsset.find(ov_asset.pid)
+    check.artesia_ingest.should == artesia_ingest
+  end
+
 end
