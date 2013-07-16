9516aec0357f787c88fb4dd252f0c70841081c7a
Joshua Wilcox <jwilcox09@gmail.com>
2013-07-16 11:30:20 -0400

    added role for users (member, scholar), removed sofia routes for now, fixed edit page for users. Closes #111 . Closes #113

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,75 @@
 ActiveAdmin.register User do
+  
+  #we don't need the admin comments for users
   config.comments = false
-  config.clear_sidebar_sections!
+  
+  #this will turn off the sidebar
+  #config.clear_sidebar_sections!
+  
+  #filters (only a select few are needed but uncomment out the below line if more are needed)
+  #preserve_default_filters!
+  filter :email
+  filter :first_name
+  filter :last_name
+  filter :role, :as => :select, :collection => [['member', 'member'], ['scholar', 'scholar']]
+  
+  #overrides the index view. remove :download_links => false if you want the download links
+  index :download_links => false do
+    column :id
+    column :first_name
+    column :last_name
+    column :email
+    column :postal_code
+    column :country
+    column :role
+    actions
+  end
+  
+  #overrides the view page format
+  show do |ad|
+    attributes_table do
+      row :first_name
+      row :last_name
+      row :email
+      row :postal_code
+      row :country
+      row :mla_updates
+      row :role
+    end
+  end
+  
+  #overrides the edit/new form
+  form do |f|
+    f.inputs "User Data" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :first_name
+      f.input :last_name
+      f.input :postal_code
+      f.input :country
+      f.input :mla_updates
+      f.input :terms_and_conditions
+      f.input :role, :as => :select, :collection => [['member', 'member'], ['scholar', 'scholar']]
+    end
+    f.actions
+  end
+  
+  controller do
+    # This code is evaluated within the controller class and overrides the exisiting methods if they are the same
+    def update
+       @user = User.find(params[:id])
+       if params[:user][:password].blank?
+         @user.update_without_password(params[:user])
+       else
+         @user.update_attributes(params[:user])
+       end
+       if @user.errors.blank?
+         redirect_to admin_users_path, :notice => "User updated successfully."
+       else
+         render :edit
+       end
+     end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,17 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :first_name, :last_name, :postal_code, :country, :mla_updates, :terms_and_conditions
+                  :first_name, :last_name, :postal_code, :country, :mla_updates, 
+                  :terms_and_conditions, :role
 
   validates :terms_and_conditions, acceptance: {allow_nil: false, accept: true, on: :create}
-  #validates_presence_of :terms_and_conditions
-  #validate :check_terms
   validates_presence_of :first_name, :message => "can't be blank"
   validates_presence_of :last_name, :message => "can't be blank"
   validates_presence_of :postal_code, :message => "can't be blank"
   validates_presence_of :country, :message => "can't be blank"
   validates_uniqueness_of :email, :message => "must be unique"
+  validates_presence_of :role, :message => "can't be blank"
+  validates_inclusion_of :role, :in => %w( member scholar ), :on => :create, :message => "role %s is not included in the list"
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
@@ -28,5 +29,4 @@ class User < ActiveRecord::Base
   def to_s
     email
   end
-  
 end
