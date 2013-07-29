32e29d3006f8dd2eb4cf25d83da5164a1347115c
Joshua Wilcox <jwilcox09@gmail.com>
2013-07-29 17:12:06 -0400

    finally fixed the cancan bug that broke hyrda models

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,28 +2,47 @@ class Ability
   include CanCan::Ability
   include Hydra::Ability
 
-  def initialize(user)
-    #
-    if user
+  # def initialize(user)
+  #   if user
+  #     
+  #     can :manage, Collection
+  #     
+  #     can [:show, :index], CustomCollection
+  #     
+  #     can [:edit, :destroy, :delete, :update], CustomCollection do |coll|
+  #       user.is_scholar? && coll.user_can_edit?(user)
+  #     end
+  #     
+  #     can [:create, :new], CustomCollection if user.is_scholar?
+  #     
+  #   else
+  #     can :manage, Collection
+  # 
+  #     can [:show, :index], CustomCollection
+  #   end
+  #   #super
+  # end
+  
+  def custom_permissions
+    if @current_user
       
       can :manage, Collection
       
       can [:show, :index], CustomCollection
       
-      can [:create, :new], CustomCollection do |coll|
-        user.is_scholar?
-      end
+      can [:create, :new], CustomCollection if @current_user.is_scholar?
+      
+      cannot [:create, :new], CustomCollection if @current_user.is_member?
       
       can [:edit, :destroy, :delete, :update], CustomCollection do |coll|
-        user.is_scholar? && coll.user_can_edit?(user)
+        @current_user.is_scholar? && coll.user_can_edit?(@current_user)
       end
       
     else
       can :manage, Collection
-
+  
       can [:show, :index], CustomCollection
     end
     
-    #super
   end
 end
