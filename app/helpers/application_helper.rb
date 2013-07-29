d95f34b0592d7d5fb2506acd7ef6bae3cda179b5
Joshua Wilcox <jwilcox09@gmail.com>
2013-07-29 14:23:10 -0400

    added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def organization_name
     Rails.configuration.organization_name
   end
+  
+  def title(page_title, options={})
+    content_for(:title, page_title.to_s)
+    return content_tag(:h1, page_title, options)
+  end
+  
 end
