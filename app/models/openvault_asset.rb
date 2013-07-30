7ee5bb128c3c8dee724b395f74e45404e3ba9b40
Joshua Wilcox <jwilcox09@gmail.com>
2013-07-30 11:57:20 -0400

    fixed typo in OpenvaultAsset model

--- app/models/openvault_asset.rb
+++ app/models/openvault_asset.rb
@@ -2,7 +2,7 @@ require 'hydra-pbcore'
 
 class OpenvaultAsset < ActiveFedora::Base
 
-  has_metadata 'uois', :type => Datastreams::UOIS
+  has_metadata 'uois', :type => Datastream::UOIS
   
   belongs_to :artesia_ingest, :property => :is_part_of, :class_name => 'ArtesiaIngest'
   
