d03c0547f003df624db33dfa7d886e98fa956b57
Joshua Wilcox <jwilcox09@gmail.com>
2013-08-01 09:44:21 -0400

    added routing specs for users model

---
+++ spec/routing/users_routing_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe UsersController do
+  describe "routing" do
+    
+    it "/me routes to user #show" do
+      get("/me").should route_to("users#show")
+    end
+    
+    it "/user/:username routes to user #show" do
+      get("/user/josh-wilcox").should route_to("users#show", :username => 'josh-wilcox')
+    end
+    
+    it "/users/edit routes to devise/registrations#edit" do
+      get("/users/edit").should route_to("devise/registrations#edit")
+    end
+    
+    it 'routes to devise/registrations#new' do
+      get("/users/sign_up").should route_to("devise/registrations#new")
+    end
+    
+    it 'routes to devise/sessions#new' do
+      get("/users/sign_in").should route_to("devise/sessions#new")
+    end
+    
+  end
+end
