785f524413afeb0863384b021051e5cc4b36b6ff
Joshua Wilcox <jwilcox09@gmail.com>
2013-08-01 10:10:44 -0400

    basics structure for catalog controller tests

---
+++ spec/controllers/catalog_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe CatalogController do
+  
+  describe "GET index" do
+    it "returns search results"
+    
+    it "renders no_results when no results found"
+  end
+  
+  describe "GET show" do
+    it "gets the catalog item when requested"
+    
+    it "return 401 when no item exists"
+  end
+  
+  describe "GET home" do
+    it "get has the mosaic items"
+    
+    it "gets the collections"
+  end
+  
+end
