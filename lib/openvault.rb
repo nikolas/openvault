23dabafcbac63271fa573b28c0a96806d74d831f
Andrew Myers <andrew_myers@wgbh.org>
2013-08-12 20:19:11 -0400

    * Adds .ng_parse_options as accessible attribute to Openvault module's class, and overwrite ng_parse_options= to raise an error if you try to set it to something other than an integer (otherwise you'll get a funky error thrown from inside of Nokogiri somewhere).
    * Adds Openvault::XML as a method _and_ a module so you can use in the same was as Nokogiri::XML.. but it uses Openvault.ng_parse_options.
    
    Closes #137.

---
+++ lib/openvault.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+
+module Openvault
+  class << self
+    attr_accessor :ng_parse_options
+
+    def ng_parse_options= val
+      raise "ng_parse_options expects an integer, #{val.class} given" unless val.kind_of? Integer
+      @ng_parse_options = val
+    end
+
+    ##
+    # Convenience method for using self.ng_parse_options when parsing xml with Nokogiri
+    def XML thing, url = nil, encoding = nil, options = nil, &block
+      options ||= self.ng_parse_options
+      Nokogiri::XML::Document.parse(thing, url, encoding, options, &block)
+    end
+  end
+
+  # Default Nokogiri parse options for Openvault.
+  self.ng_parse_options = Nokogiri::XML::ParseOptions::STRICT
+end
---
+++ spec/lib/openvault_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'openvault'
+
+describe Openvault do
+
+  describe '.ng_parse_options' do
+
+    it 'raises an error when passed a non-integer' do
+      expect{ Openvault.ng_parse_options = "foo" }.to raise_error
+    end
+
+    it 'defaults to Nokogiri::XML::ParseOptions::STRICT' do
+      Openvault.ng_parse_options.should == Nokogiri::XML::ParseOptions::STRICT
+    end
+  end
+
+  describe '::XML' do
+
+    let(:valid_xml) { File.read("#{fixture_path}/openvault/valid_xml.xml") }
+    let(:invalid_xml) { File.read("#{fixture_path}/openvault/invalid_xml.xml") }
+
+    it 'returns an instance of Nokogiri::XML::Document' do
+      Openvault::XML(valid_xml).should be_a_kind_of(Nokogiri::XML::Document)
+    end
+
+    context 'when Openvault.ng_parse_options is set to "strict" mode' do
+      before(:each) do
+        Openvault.ng_parse_options = Nokogiri::XML::ParseOptions::STRICT
+      end
+
+      it 'will raise an error when Openvault.ng_parse_options is set to STRICT, and xml is invalid' do
+        expect{ Openvault::XML(invalid_xml) }.to raise_error
+      end
+
+      it 'will not raise an error when Openvault.ng_parse_options is set to STRICT, and xml is valid' do
+        expect{ Openvault::XML(valid_xml) }.to_not raise_error
+      end
+    end
+  end
+    
+end
