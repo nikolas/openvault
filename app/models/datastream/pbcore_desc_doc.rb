65fb5c00b7b79ea9416c62bb5fe66b2b990be255
Andrew Myers <andrew_myers@wgbh.org>
2013-10-25 12:07:00 -0400

    Removes solr indexing of om terms in PbcoreDescDoc datastream. Closes #194.

--- app/models/datastream/pbcore_desc_doc.rb
+++ app/models/datastream/pbcore_desc_doc.rb
@@ -5,29 +5,29 @@ class PbcoreDescDoc < ActiveFedora::OmDatastream
 
     # Terminology for <pbcoreTitle>
 
-    t.series_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Series"}, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.series_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Series"})
 
-    t.program_title(:path => "pbcoreTitle", :attributes=>{:titleType=>"Program"}, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.program_title(:path => "pbcoreTitle", :attributes=>{:titleType=>"Program"})
 
-    t.chapter_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Chapter" }, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.chapter_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Chapter" })
 
-    t.episode_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Episode" }, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.episode_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Episode" })
     
-    t.element_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Element" }, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.element_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Element" })
     
-    t.clip_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Clip" }, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.clip_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Clip" })
     
-    t.label(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Label" }, :index_as => [:stored_searchable, :displayable])
+    t.label(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Label" })
     
-    t.segment_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Segment" }, :index_as => [:stored_searchable, :displayable])
+    t.segment_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Segment" })
     
-    t.subtitle(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Subtitle" }, :index_as => [:stored_searchable, :displayable])
+    t.subtitle(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Subtitle" })
     
-    t.track_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Track" }, :index_as => [:stored_searchable, :displayable])
+    t.track_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Track" })
 
-    t.image_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Image" }, :index_as => [:stored_searchable, :displayable])
+    t.image_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Image" })
 
-    t.translation_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Translation" }, :index_as => [:stored_searchable, :displayable])
+    t.translation_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Translation" })
 
     # Term for inspecting all the titles present in the XML.
     t.all_titles(:path => "pbcoreTitle") {
@@ -37,23 +37,23 @@ class PbcoreDescDoc < ActiveFedora::OmDatastream
     t.asset_type(:path => "pbcoreAssetType")
 
 
-    t.category(:path=>"pbcoreSubject", :attributes=>{:subjectType=>"Category"},:index_as => [:facetable, :displayable])
+    t.category(:path=>"pbcoreSubject", :attributes=>{:subjectType=>"Category"})
 
-    t.asset_date(:path=>"pbcoreAssetDate", :type => :string, :index_as => [:facetable, :stored_searchable, :displayable])
+    t.asset_date(:path=>"pbcoreAssetDate", :type => :string)
 
     # Terminology for <pbcoreDescription>
 
-    t.summary(:path => 'pbcoreDescription', :attributes => {:descriptionType => "Summary"}, :index_as => [:stored_searchable, :displayable])
+    t.summary(:path => 'pbcoreDescription', :attributes => {:descriptionType => "Summary"})
     
     # Creator names and roles
     t.creator(:path=>"pbcoreCreator") do
       t.creator
       t.role_(:path=>"creatorRole")
     end
-    t.creator_name(:ref=>[:creator, :creator], :type => :string, :index_as => [:stored_searchable, :displayable])
-    t.creator_role(:ref=>[:creator, :role], :type => :string, :index_as => [:stored_searchable, :displayable])
+    t.creator_name(:ref=>[:creator, :creator], :type => :string)
+    t.creator_role(:ref=>[:creator, :role], :type => :string)
 
-    t.all_descriptions(:path => 'pbcoreDescription', :index_as => [:stored_searchable, :displayable]) {
+    t.all_descriptions(:path => 'pbcoreDescription') {
       t.type(:path => {:attribute => 'descriptionType'})
     }
 
