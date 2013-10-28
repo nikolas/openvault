a1c2590ffcefe5dc758d9bc38c7cff572a77e03f
Joshua Wilcox <jwilcox09@gmail.com>
2013-10-28 14:08:50 -0400

    added to_solr methods and url methods to transcript and image models

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,14 @@ class Image < OpenvaultAsset
   belongs_to :video, :property => :image_video
   belongs_to :audio, :property => :image_audio
   
+  def to_solr(solr_document={}, options={})
+    super(solr_document, options)
+    Solrizer.insert_field(solr_document, "image_path", self.image_file, :displayable)
+    return solr_document
+  end
+  
+  def image_file
+    "http://media.wgbh.org/streaming/images/#{self.pid}.jpg"
+  end
+  
 end
--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -5,8 +5,14 @@ class Transcript < OpenvaultAsset
   belongs_to :audio, :property => :transcript_audio
   # belongs_to :asset_collection, :property => :is_part_of_collection
   
-  # def self.model_name
-  #   OpenvaultAsset.model_name
-  # end
+  def to_solr(solr_document={}, options={})
+    super(solr_document, options)
+    Solrizer.insert_field(solr_document, "transcript_path", self.transcript_file, :displayable)
+    return solr_document
+  end
+  
+  def transcript_path
+    "http://media.wgbh.org/streaming/transcripts/#{self.pid}.xml"
+  end
   
 end
