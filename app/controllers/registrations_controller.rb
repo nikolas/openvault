4959ab98fb39f5bc4e2c3fcae134a2bde417008e
Andrew Myers <andrew_myers@wgbh.org>
2013-12-09 14:32:28 -0500

    Fixes #224 Saving user profiles was reverting user's role back to "member" every time.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,14 +17,8 @@ class RegistrationsController < Devise::RegistrationsController
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case his password changed
       sign_in @user, :bypass => true
-
-      debugger
-
       redirect_to after_update_path_for(@user)
     else
-
-      debugger
-
       render "edit"
     end
 
