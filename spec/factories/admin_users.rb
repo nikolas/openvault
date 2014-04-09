7d443ef3a8b14bac012ec97bcb5aa483278b65c2
Renae Bair <renaebair@gmail.com>
2014-04-09 10:53:41 -0400

    Fixes #217 Admin created user can login
    Admin can create a new user within activeadmin, that user can then login
    successfully.

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,12 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+# # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
-  factory :admin_user do
+# FactoryGirl.define do
+#   factory :admin_user do
     
-    email { "#{first_name}_#{last_name}@#{Faker::Internet.domain_name}".downcase }
+#     email { "#{first_name}_#{last_name}@#{Faker::Internet.domain_name}".downcase }
     
-    password { Faker::Lorem.characters 8 }
-    password_confirmation { "#{password}" }
+#     password { Faker::Lorem.characters 8 }
+#     password_confirmation { "#{password}" }
     
-  end
-end
+#   end
+# end
