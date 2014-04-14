b80ac5f3eafc23db64ccbd7daaf078ff9c8e67ca
Renae Bair <renaebair@gmail.com>
2014-04-14 12:41:21 -0400

    #232 remove save_and_open_page

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -29,7 +29,6 @@ feature 'User searches' do
     #need to fix facets display for this to work
     # scenario 'and they filter with a facet' do
 #       search({q: 'Nova'})
-#       save_and_open_page
 #       filter_click({filter: 'Robin Bates'})
 #       expect(page).to have_css('#appliedParams .filterValue a', text: 'Robin Bates')
 #     end
