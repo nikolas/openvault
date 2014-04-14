f5519c90f293d84c4944dc51eb6b97c88054ca70
Andrew Myers <andrew_myers@wgbh.org>
2014-04-14 10:57:39 -0400

    Changes OpenvaultAsset#to_solr. This affects how solr documents are saved, and so will require re-indexing to see the effects. To re-index, you can try ActiveFedora::Base.reindex_everything. Not sure if that behaves as expected though. You can also re-save ActiveFedora models. Adds the browse_by_title view, which should have been in an earlier commit.

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,15 +1,35 @@
 class SeriesController < CatalogController
 
   def browse_by_title
-    @all_series = Series.all
-    @all_series.sort_by! {|series| series.title}
+    @series_results = Blacklight.solr.select(params: {q: "has_model_ssim:*Series*", rows: 10000})['response']['docs']
+
+    # You can sort using Solr, but it defaults to being case sensitive, and putting numbers before letters.
+    # This custom sort puts letters before numbers and is case insensitive.
+    @series_results.sort! do |a, b|
+      cmp_a = (a['title_tesim'].nil? || a['title_tesim'].empty?) ? '' : a['title_tesim'].first
+      cmp_b = (b['title_tesim'].nil? || b['title_tesim'].empty?) ? '' : b['title_tesim'].first
+
+      case
+      # push titles beginning with numbers to the end
+      when (/^[0-9]/ =~ cmp_a) && !(/^[0-9]/ =~ cmp_b)
+        1
+      when !(/^[0-9]/ =~ cmp_a) && (/^[0-9]/ =~ cmp_b)
+        -1
+      else
+        cmp_a.upcase <=> cmp_b.upcase
+      end
+    end
+
     respond_to do |format|
       format.html {render :browse_by_title}
     end
   end
   
   def show
-    @response, @document = get_solr_document_by_slug(params[:id])    
+
+    @response, @document = get_solr_response_for_doc_id params[:id]
+
+    # @response, @document = get_solr_document_by_id(params[:id])
     #@repspones, @document = get_solr_response_for_doc_id
     
     @rel = get_related_content(params[:id])
@@ -80,6 +100,7 @@ class SeriesController < CatalogController
   
   def get_programs(document=nil)
     progs = []
+
     unless document[:programs_ssm].nil?
       document[:programs_ssm].each do |prog|
         progs << get_only_solr_document_by_slug(prog.to_s)
