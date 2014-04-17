a5ef8770064fe229ff2dce0d624b6f823a7492e6
Ted O'Meara <ted@tedomeara.com>
2014-04-17 17:22:57 -0400

    #284 - created model for carousel items to display on the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140409000125) do
+ActiveRecord::Schema.define(:version => 20140417195418) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -28,6 +28,24 @@ ActiveRecord::Schema.define(:version => 20140409000125) do
   add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
     t.string   "document_id"
@@ -35,6 +53,17 @@ ActiveRecord::Schema.define(:version => 20140409000125) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "user_type"
+  end
+
+  create_table "carousel_items", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "url"
+    t.string   "image"
+    t.boolean  "enabled",    :default => false
+    t.integer  "position"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
   create_table "collections", :force => true do |t|
