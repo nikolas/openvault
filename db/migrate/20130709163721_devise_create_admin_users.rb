1ef8fccc715c09f4adb03aec2ec922b889bff80e
Peter Jackson <pete@peteonrails.com>
2014-05-19 22:02:24 -0400

    Correct superclass and comment out data creation

--- db/migrate/20130709163721_devise_create_admin_users.rb
+++ db/migrate/20130709163721_devise_create_admin_users.rb
@@ -1,11 +1,11 @@
 class DeviseCreateAdminUsers < ActiveRecord::Migration
-  class AdminUser < User
+  class AdminUser < ActiveRecord::Base
   end
-  
+
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
+    # AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
   end
 
   def change
