1c5d7c467882377b9241e919b33b8086bbbd2535
Peter Jackson <pete@peteonrails.com>
2014-05-28 00:17:09 -0400

    Change some user factory stuff overr to let() format

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,33 +2,25 @@ require 'spec_helper'
 
 describe UsersController do
 
+  let(:scholar) { FactoryGirl.create(:scholar_user) }
+  let(:member) { FactoryGirl.create(:user) }
+  let(:org) { orgs.first }
+  let(:orgs) { FactoryGirl.create_list(:org, 3) }
+  before(:each) { scholar.save and member.save and org.save }
+
   describe "#scholar" do
     it 'responds with 200 success if user is a a scholar' do
-      scholar = create(:user, role: "scholar")
       get :scholar, username: scholar.username
       expect(response.code).to eq "200"
     end
 
-
     it 'responds with 404 if user is not a scholar' do
-      not_scholar = create(:user, role: "member")
-      expect{get :scholar, username: not_scholar.username}.to raise_error
+      expect{get :scholar, username: member.username}.to raise_error
     end
   end
 
-
   describe 'POST add_orgs' do
-
-    before :each do
-      @user = create(:user)
-    end
-
     describe 'with one org' do
-
-      before :each do
-        @org = create(:org)
-      end
-
       pending 'saves association between User instance and a the Org instance' do
         # post :add_orgs, id: @user.to_param, org_ids: @orgs.map{ |org| org.id }
         # TODO: verify respose -- see controller for what to expect
@@ -37,15 +29,11 @@ describe UsersController do
 
     describe 'with multiple orgs' do
 
-      before :each do
-        @orgs = create_list(:org, 3)
-      end
-
       pending 'saves assocaition between User instance and multiple Org instances' do
         # post :add_orgs, id: @user.to_param, org_ids: @orgs.map{ |org| org.id }
         # TODO: verify respose -- see controller for what to expect
       end
     end
 
-  end  
+  end
 end
