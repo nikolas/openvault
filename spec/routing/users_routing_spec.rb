008257030f70d55250d5ff0986e81b18d40be25c
Peter Jackson <pete@peteonrails.com>
2014-05-29 19:49:28 -0400

    Fix incorrect routing spec for Devise::RegistrationsController, which has been overridden so that we can allow updates to user profiles without passwords

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -2,11 +2,10 @@ require "spec_helper"
 
 describe UsersController do
   describe "routing" do
-    
     it "/me routes to user #show" do
       get("/me").should route_to("users#show")
     end
-    
+
     it "/user/:username routes to user #show" do
       get("/user/josh-wilcox").should route_to("users#show", :username => 'josh-wilcox')
     end
@@ -14,18 +13,17 @@ describe UsersController do
     it "routes /scholar/:username to user#scholar" do
       get('/scholar/foo-bar').should route_to("users#scholar", username: 'foo-bar')
     end
-    
-    it "/users/edit routes to devise/registrations#edit" do
-      get("/users/edit").should route_to("devise/registrations#edit")
+
+    it "/users/edit routes to overridden registrations#edit" do
+      get("/users/edit").should route_to("registrations#edit")
     end
-    
-    it 'routes to devise/registrations#new' do
-      get("/users/sign_up").should route_to("devise/registrations#new")
+
+    it 'routes to overridden registrations#new' do
+      get("/users/sign_up").should route_to("registrations#new")
     end
-    
+
     it 'routes to devise/sessions#new' do
       get("/users/sign_in").should route_to("devise/sessions#new")
     end
-    
   end
 end
