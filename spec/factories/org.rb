64a233146ab6c1cbaaf6a338723b6a9347ecb2d9
Andrew Myers <andrew_myers@wgbh.org>
2014-06-11 22:20:09 -0400

    Simplifies :admin factory. Changes Faker data in Org factory.

--- spec/factories/org.rb
+++ spec/factories/org.rb
@@ -3,8 +3,13 @@ FactoryGirl.define do
   # A factory for instances of Custom Collections model.
   factory :org do
 
-    name { Faker::Name.title }
-    desc { Faker::Lorem.paragraph }
+    name { Faker::Company.name }
+    desc do
+      bs = []
+      rand(2..5).times{ bs << Faker::Company.bs }
+      last_bs = bs.pop
+      "We " + bs.join(', ') + ' and ' + last_bs
+    end
 
   end
 end
