ff6748e72dbf526656638b33422bcd07f20b2583
Andrew Myers <andrew_myers@wgbh.org>
2014-06-13 14:56:28 -0400

    Monkey patches ActiveRecord to cause Capybara to share db connection so that transactional fixtures can be turned on with RSpec, eliminating need for DatabseCleaner to run in after(:each) or after(:context) hooks. NOTE: DatabaseCleaner is still used to truncate db in before(:suite) hook.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,20 +51,11 @@ RSpec.configure do |config|
   config.include SearchSteps
   config.include CustomCollectionSteps
 
-  config.use_transactional_fixtures = false
-
+  config.use_transactional_fixtures = true
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+    # DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
   end
 
   config.fixture_path = File.expand_path("../fixtures", __FILE__)
