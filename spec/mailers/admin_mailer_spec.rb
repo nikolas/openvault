650078a919d4ff54a436da11983568212abc30a0
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-07-18 14:24:01 -0400

    Fixes occasional issue with admin mailer where the fixture was not able to be found

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -2,14 +2,15 @@ require "spec_helper"
 require "#{RSpec.configuration.fixture_path}/pbcore/load_fixtures"
 
 describe AdminMailer do
-	before do
-	  @user = create(:user)
+  before do
+    @user = create(:user)
     @ov_asset = OpenvaultAsset.new(:pid => 'test:1234')
+    Fixtures.cwd("#{fixture_path}/pbcore")
     @ov_asset.pbcore.ng_xml = Fixtures.use('artesia/rock_and_roll/video_1.xml').ng_xml
     @ov_asset.save
-  	@artifact = Artifact.create(:pid => @ov_asset.pid)
-  	@artifact.request!(@user)
-  	@artifact.save
+    @artifact = Artifact.create(:pid => @ov_asset.pid)
+    @artifact.request!(@user)
+    @artifact.save
   end
 
   it "should email an admin when a request is made" do
