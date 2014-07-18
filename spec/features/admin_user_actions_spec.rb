00e3b787687293f686d1175516db505dd75c9e42
Andrew Myers <andrew_myers@wgbh.org>
2014-07-18 15:02:13 -0400

    Modifies spec for 'admin creates new user'.

--- spec/features/admin_user_actions_spec.rb
+++ spec/features/admin_user_actions_spec.rb
@@ -12,35 +12,25 @@ feature 'Admin creates a user and that user can login' do
     login_as @admin
   end
 
-  scenario 'admin can login and visit admin dashboard', broken: true do
+  scenario 'admin can login and visit admin dashboard' do
     go_here "/admin"
     expect(page).to have_content('Welcome to ActiveAdmin')
   end
 
-  scenario 'admin creates a new user' do
-    first = 'William'
-    email = 'williamblake@gmail.com'
-    password = 'wblake123456'
-    
+  scenario 'admin creates a new user', focus: true do
     go_here "/admin/users"
     click_link "New User"
-    fill_in 'user_first_name', :with => first
-    fill_in 'user_last_name', :with => 'Blake'
-    fill_in 'user_email', :with => email
-    fill_in 'user_password', :with => password
-    fill_in 'user_password_confirmation', :with => password
+
+    @user_attrs = attributes_for(:user)
+    fill_in 'user_first_name', with: @user_attrs[:first_name]
+    fill_in 'user_last_name', with: @user_attrs[:last_name]
+    fill_in 'user_email', with: @user_attrs[:email]
+    fill_in 'user_password', with: @user_attrs[:password]
+    fill_in 'user_password_confirmation', with: @user_attrs[:password]
     check 'user_terms_and_conditions'
-    select("scholar",:from=> "user_role")
+    select(@user_attrs[:role],from: "user_role")
     click_button "Create User"
     expect(page).to have_content("User was successfully created.")
-    click_link "Logout"
-    click_link "Log In"
-    fill_in 'user_email', :with => email
-    fill_in 'user_password', :with => password
-    click_button 'Sign in'
-    expect(page).to have_content("Welcome back #{first}"), # TODO: when this test is working, kill the verbosity.
-      'After capybara timeout, still not logged in: ajax calls still active: ' \
-      + page.evaluate_script('jQuery.active').to_s \
-      + '. Page content: ' + page.body
+    expect(User.where(email: @user_attrs[:email]).count).to eq 1
   end
 end
