0b92fc0896c3914dc9f8df0b6e419c6f84182d13
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-07-25 16:41:31 -0400

    Another, but not sure it is covered by any tests.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -28,24 +28,6 @@ class VideosController < ContentController
       end
    # end
   end
-
-  def embed
-    @response, @document = get_solr_response_for_doc_id    
-    @width = params[:width].try(:to_i) || 640
-    @height = params[:height].try(:to_i) || (3 * @width / 4)
-    respond_to do |format|
-      format.html {render :layout => 'embed' }
-      
-      # Add all dynamically added (such as by document extensions)
-      # export formats.
-      @document.export_formats.each_key do | format_name |
-        # It's important that the argument to send be a symbol;
-        # if it's a string, it makes Rails unhappy for unclear reasons. 
-        format.send(format_name.to_sym) { render :text => @document.export_as(format_name) }
-      end
-      
-    end
-  end
   
   protected
   
