0ffaea3ff46dbc41889a542b844bc644f6be97b7
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-07-28 18:00:58 -0400

    Test for appropriate 404s

--- spec/features/catalog/show_spec.rb
+++ spec/features/catalog/show_spec.rb
@@ -14,4 +14,10 @@ feature "User visits catalog#show" do
     visit "/catalog/#{@program.pid}"
     expect(page).to have_content("#{@program.pbcore.asset_date.first}")
   end
+  
+  scenario "non-existent returns 404" do
+    visit "/catalog/nope:12345"
+    expect(page.status_code).to eq(404)
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
 end
