286ec0a08a3b3bedf43a5b9260497df84194650b
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-08-13 08:57:20 -0400

    Changes search to audio.title.first since audio.title was searching for an array

--- spec/features/catalog/index_spec.rb
+++ spec/features/catalog/index_spec.rb
@@ -12,21 +12,21 @@ feature "User views search results" do
 
   scenario "metadata includes title" do
     visit "/"
-    fill_in 'q', with: @audio.title
+    fill_in 'q', with: @audio.title.first
     click_button 'search'
     expect(page).to have_content("#{@audio.title.first}")
   end
 
   scenario "metadata includes media type" do
     visit "/"
-    fill_in 'q', with: @audio.title
+    fill_in 'q', with: @audio.title.first
     click_button 'search'
     expect(page).to have_content("Media Type:#{@audio.to_solr['active_fedora_model_ssi']}")
   end
 
   scenario "metadata includes date" do
     visit "/"
-    fill_in 'q', with: @audio.title
+    fill_in 'q', with: @audio.title.first
     click_button 'search'    
     expect(page).to have_content("Date:#{@audio.asset_date}")
   end
