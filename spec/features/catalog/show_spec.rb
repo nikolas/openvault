eea91cb2f7ef49053674b831d3e4a75eb0e1be0a
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-08-15 12:15:37 -0400

    Test asset rendering. Passes, but does not reproduce #621, though.

--- spec/features/catalog/show_spec.rb
+++ spec/features/catalog/show_spec.rb
@@ -3,21 +3,55 @@ require "#{RSpec.configuration.fixture_path}/pbcore/load_fixtures"
 require 'openvault/pbcore'
 
 feature "User visits catalog#show" do
+
   before(:all) do 
     Fixtures.cwd("#{fixture_path}/pbcore")
-    @video = Video.new
-    @video.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/video_1.xml")
-    @video.save!
+  end
+  
+  def setup(type, fixture_path)
+    asset = type.new
+    asset.pbcore.ng_xml = Fixtures.raw(fixture_path)
+    asset.save!
+    return asset
+  end
+  
+  def expects(asset)
+    visit "/catalog/#{asset.pid}"
+    expect(page).to have_content("#{asset.date_portrayed}")
+  end
+  
+  
+  scenario "series works" do
+    series = setup(Series, "artesia/rock_and_roll/series_1.xml")
+    expects(series)
+  end
+  
+  scenario "program works" do
+    program = setup(Program, "artesia/rock_and_roll/program_1.xml")
+    expects(program)
+  end
+  
+  scenario "video works" do
+    video = setup(Video, "artesia/patriots_day/video_1.xml")
+    expects(video)
+  end
+  
+  scenario "audio works" do
+    audio = setup(Audio, "artesia/patriots_day/audio_1.xml")
+    expects(audio)
   end
 
-  scenario "metadata includes date" do
-    visit "/catalog/#{@video.pid}"
-    expect(page).to have_content("#{@video.date_portrayed}")
+  scenario "image works" do
+    image = setup(Series, "artesia/rock_and_roll/image_1.xml")
+    expects(image)
   end
+  
+  # TODO: transcript
   
   scenario "non-existent returns 404" do
     visit "/catalog/nope:12345"
     expect(page.status_code).to eq(404)
     expect(page).to have_content("The page you were looking for doesn't exist.")
   end
+
 end
