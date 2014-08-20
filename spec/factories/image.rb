565add12e49f7701d909484fb5b6201fa2418c3a
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-08-20 18:31:04 -0400

    Relationship tests: failing right now.

---
+++ spec/factories/image.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  
+  factory :image do
+    # TODO?
+  end
+  
+end
--- spec/models/relate_asset_spec.rb
+++ spec/models/relate_asset_spec.rb
@@ -3,22 +3,81 @@ require 'spec_helper'
 describe 'relate_asset' do
 
   before(:each) do
-    @program = FactoryGirl.create :program
-    @video = FactoryGirl.create :video
-    @audio = FactoryGirl.create :audio
-    @series = FactoryGirl.create :series
+    @all = [
+      @program = FactoryGirl.create(:program),
+      @video = FactoryGirl.create(:video),
+      @audio = FactoryGirl.create(:audio),
+      @series = FactoryGirl.create(:series),
+      @image = FactoryGirl.create(:image),
+      @transcript = FactoryGirl.create(:transcript)
+    ]
   end
   
+  # All valid relationships are symmetrical.
   def no_error(a,b)
     expect {a.relate_asset b}.not_to raise_error
+    expect {b.relate_asset a}.not_to raise_error
+  end
+  def yes_error(a,b)
+    expect {a.relate_asset b}.to raise_error
+    expect {b.relate_asset a}.to raise_error
   end
   
-  describe Program do
-    it 'relates well' do
-      no_error @program, @video
-      no_error @program, @audio
-      no_error @program, @series
+  def no_errors(parent,children)
+    children.each do |child|
+      no_error parent, child
     end
+  end
+  def yes_errors(parent,children)
+    children.each do |child|
+      yes_error parent, child
+    end
+  end
+  
+  def relates (parent, good_children)
+    bad_children = @all - good_children
+    no_errors parent, good_children
+    yes_errors parent, bad_children
+  end
+  
+  it Series do
+    relates @series, [
+      @program, 
+      @image, # a logo?
+      @audio, @video # perhaps promo materials for the series as a whole.
+    ]
+  end
+  
+  it Program do
+    relates @program, [
+      @series, # parent
+      @video, @audio, @image
+      # but not transcript, which would always belong to a video or audio?
+    ]
+  end
+  
+  it Video do
+    relates @video, [
+      @series, @program, #parents
+      @image, @transcript]
+  end
+  
+  it Audio do
+    relates @audio, [
+      @series, @program, #parents 
+      @transcript]
+  end
+  
+  it Image do
+    relates @image, [
+      @series, @program, @video #parents
+    ]
+  end
+  
+  it Transcript do
+    relates @transcript, [
+      @video, @audio #parents
+     ]
   end
   
 end
