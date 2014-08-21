a085a934a4d1da4f2021ad53be7986377c364fa7
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-08-21 14:58:21 -0400

    Adds tests for the transcripts helper and for video page to ensure transcripts are being displayed.

--- spec/features/catalog/show_spec.rb
+++ spec/features/catalog/show_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 require "#{RSpec.configuration.fixture_path}/pbcore/load_fixtures"
 require 'openvault/pbcore'
+include TranscriptsHelper
+Capybara.asset_host = 'http://localhost:3000'
 
 feature "User visits catalog#show" do
   before(:all) do 
     Fixtures.cwd("#{fixture_path}/pbcore")
-    @video = Video.new
-    @video.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/video_1.xml")
-    @video.save!
+    @video = Video.create
+    @video.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/video_3.xml")
+    @transcript = Transcript.new
+    @transcript.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/transcript_1.xml")
+    @transcript.tei.ng_xml = Fixtures.raw("artesia/patriots_day/transcript_tei_1.xml")
+    @video.transcripts << @transcript
+    @video = OpenvaultAsset.find(@video.pid, cast: true)
   end
 
   scenario "metadata includes date" do
@@ -15,6 +21,12 @@ feature "User visits catalog#show" do
     expect(page).to have_content("#{@video.date_portrayed}")
   end
   
+  scenario "page displays transcript" do
+    visit "/catalog/#{@video.pid}"
+    expect(page).to have_content("TRANSCRIPT")
+    expect(page).to have_content("Interviewer:")
+  end
+
   scenario "non-existent returns 404" do
     visit "/catalog/nope:12345"
     expect(page.status_code).to eq(404)
