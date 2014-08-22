4ba77494dbbfca44b3e7268687d7d6d44d043056
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-08-22 15:27:49 -0400

    Test classifier using factory.

--- spec/lib/openvault/pbcore/asset_classifier_spec.rb
+++ spec/lib/openvault/pbcore/asset_classifier_spec.rb
@@ -4,28 +4,40 @@ require "#{RSpec.configuration.fixture_path}/pbcore/load_fixtures"
 
 describe Openvault::Pbcore::AssetClassifier do
 
-  before :all do
-    Fixtures.cwd "#{fixture_path}/pbcore"
-  end
-
-  def classify(path)
-    fixture = Fixtures.use(path)
-    Openvault::Pbcore::AssetClassifier.classify(fixture)
+  describe 'factories' do
+    
+    def classify(object)
+      Openvault::Pbcore::AssetClassifier.classify(object.pbcore)
+    end
+    
+    it Series do
+      expect(classify build :series).to be(Series)
+    end
+    
   end
   
-  def expect_class(klass, fixture_paths)
-    fixture_paths.each do |path|
-      begin
-        expect(classify(path)).to be(klass),
-          "Expected #{path} to be classified as #{klass}"
-      rescue RuntimeError => e
-        fail "Expected #{path} to be classified as #{klass}, instead of throwing '#{e}'"
+  
+  describe 'fixtures' do
+    
+    before :all do
+      Fixtures.cwd "#{fixture_path}/pbcore"
+    end
+
+    def classify(path)
+      fixture = Fixtures.use(path)
+      Openvault::Pbcore::AssetClassifier.classify(fixture)
+    end
+
+    def expect_class(klass, fixture_paths)
+      fixture_paths.each do |path|
+        begin
+          expect(classify(path)).to be(klass),
+            "Expected #{path} to be classified as #{klass}"
+        rescue RuntimeError => e
+          fail "Expected #{path} to be classified as #{klass}, instead of throwing '#{e}'"
+        end
       end
     end
-  end
-  
-  
-  describe 'good classifications' do
     
     it 'for Series' do
       expect_class Series, [
@@ -62,14 +74,15 @@ describe Openvault::Pbcore::AssetClassifier do
         'artesia/rock_and_roll/transcript_1.xml']
     end
     
+    describe 'exception' do
+      it 'is raised if multiple models apply' do
+        expect {classify('artesia/joyce_chen/multiple_models.xml')}.to \
+          raise_error("Multiple matching AF-models: [Video, Image]")
+      end
+      # TODO: test no classifications
+    end
+    
   end
 
-  describe 'exception' do
-    it 'is raised if multiple models apply' do
-      expect {classify('artesia/joyce_chen/multiple_models.xml')}.to \
-        raise_error("Multiple matching AF-models: [Video, Image]")
-    end
-    # TODO: test no classifications
-  end
   
 end
