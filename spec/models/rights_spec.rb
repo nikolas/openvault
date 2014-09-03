6b213e31799475e87b53668c459547cbe033109e
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-09-03 15:21:09 -0400

    Implement and test rights accessors.

---
+++ spec/models/rights_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require "#{RSpec.configuration.fixture_path}/pbcore/load_fixtures"
+
+describe 'Rights methods' do
+
+  Fixtures.cwd("#{fixture_path}/pbcore")
+  asset = OpenvaultAsset.new # no change: local variable is fine.
+  asset.pbcore.ng_xml = Fixtures.use('pbcore_rights.xml').ng_xml
+
+  %w(rights coverage credit holder note type).each do |method|
+    it method do
+      expect(asset.pbcore.rights.send(method)).to eq ["some #{method}"]
+    end
+  end
+  
+end
