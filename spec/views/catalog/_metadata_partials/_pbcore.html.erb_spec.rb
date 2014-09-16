66dc61440f6421dcb8a525b4dcebfc7b65d12a72
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-09-16 15:53:43 -0400

    Actually test that content is rendered.

--- spec/views/catalog/_metadata_partials/_pbcore.html.erb_spec.rb
+++ spec/views/catalog/_metadata_partials/_pbcore.html.erb_spec.rb
@@ -1,14 +1,25 @@
 require "spec_helper"
 
 describe "_pbcore.html.erb" do
+  it "works with Videos" do
+    @ov_asset = create(:video)
+    @ov_asset.program = create(:program)
+    @ov_asset.series = create(:series)
+    render partial: "catalog/_metadata_partials/pbcore.html.erb"
+    expect(rendered).to include @ov_asset.program.title
+    expect(rendered).to include @ov_asset.series.title
+  end
+  
   it "works with Programs" do
     @ov_asset = create(:program)
     @ov_asset.series = create(:series)
     render partial: "catalog/_metadata_partials/pbcore.html.erb"
+    expect(rendered).to include @ov_asset.series.title 
   end
   
   it "works with Series" do
     @ov_asset = create(:series)
     render partial: "catalog/_metadata_partials/pbcore.html.erb"
+    # No parents: nothing to test.
   end
 end
