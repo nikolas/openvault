9e003d6464cd692bd4ef7603019b8af3c816ec34
Anthony Vatousiou <anthony_vatousiou@ml-20015524.wgbh.org>
2014-09-17 09:17:30 -0400

    Gets the facet for series displaying (but not filtering)

--- app/models/video.rb
+++ app/models/video.rb
@@ -14,6 +14,7 @@ class Video < OpenvaultAsset
     Solrizer.insert_field(solr_document, "video_url", self.video_url, :displayable)
     Solrizer.insert_field(solr_document, "video_images", self.video_images, :displayable)
     Solrizer.insert_field(solr_document, "video_transcripts", self.video_transcripts, :displayable)
+    Solrizer.insert_field(solr_document, "series", self.series.title, :facetable) if self.series
     return solr_document
   end
 
