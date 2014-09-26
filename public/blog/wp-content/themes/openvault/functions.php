7a8ad338ef8254f1067406739b0a7d9d5b798eb2
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-09-26 13:14:57 -0400

    Get rid of WP.

--- public/blog/wp-content/themes/openvault/functions.php
+++
@@ -1,2 +0,0 @@
-<?php wp_oembed_add_provider( 'http://openvault.wgbh.org/catalog/*', 'http://openvault2.wgbh.org/catalog/oembed' ); ?>
-<?php wp_oembed_add_provider( 'http://openvault2.wgbh.org/catalog/*', 'http://openvault2.wgbh.org/catalog/oembed' ); ?>
