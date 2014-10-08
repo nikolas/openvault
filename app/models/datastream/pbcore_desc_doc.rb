b5c916db9dbcbd47da58ae3f4454a61f9d72e5f4
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-10-08 12:22:16 -0400

    fix hash rockets.

--- app/models/datastream/pbcore_desc_doc.rb
+++ app/models/datastream/pbcore_desc_doc.rb
@@ -1,91 +1,91 @@
 class PbcoreDescDoc < ActiveFedora::OmDatastream
 
   set_terminology do |t|
-    t.root(:path=>"pbcoreDescriptionDocument")
+    t.root(path: "pbcoreDescriptionDocument")
 
-    t.all_ids(:path => "pbcoreIdentifier", :index_as => [:stored_searchable]) {
-      t.source(:path => {:attribute => 'source'})
+    t.all_ids(path: "pbcoreIdentifier", index_as: [:stored_searchable]) {
+      t.source(path: {attribute: 'source'})
     }
 
     # Terminology for <pbcoreTitle>
 
-    t.series_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Series"})
+    t.series_title(path: "pbcoreTitle", attributes: { titleType: "Series"})
 
-    t.program_title(:path => "pbcoreTitle", :attributes=>{:titleType=>"Program"})
+    t.program_title(path: "pbcoreTitle", attributes: {titleType: "Program"})
 
-    t.chapter_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Chapter" })
+    t.chapter_title(path: "pbcoreTitle", attributes: { titleType: "Chapter" })
 
-    t.episode_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Episode" })
+    t.episode_title(path: "pbcoreTitle", attributes: { titleType: "Episode" })
     
-    t.element_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Element" })
+    t.element_title(path: "pbcoreTitle", attributes: { titleType: "Element" })
     
-    t.clip_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Clip" })
+    t.clip_title(path: "pbcoreTitle", attributes: { titleType: "Clip" })
     
-    t.label(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Label" })
+    t.label(path: "pbcoreTitle", attributes: { titleType: "Label" })
     
-    t.segment_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Segment" })
+    t.segment_title(path: "pbcoreTitle", attributes: { titleType: "Segment" })
     
-    t.subtitle(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Subtitle" })
+    t.subtitle(path: "pbcoreTitle", attributes: { titleType: "Subtitle" })
     
-    t.track_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Track" })
+    t.track_title(path: "pbcoreTitle", attributes: { titleType: "Track" })
 
-    t.image_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Image" })
+    t.image_title(path: "pbcoreTitle", attributes: { titleType: "Image" })
 
-    t.translation_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Translation" })
+    t.translation_title(path: "pbcoreTitle", attributes: { titleType: "Translation" })
 
     # Term for inspecting all the titles present in the XML.
-    t.all_titles(:path => "pbcoreTitle") {
-      t.type(:path => {:attribute => 'titleType'})
+    t.all_titles(path: "pbcoreTitle") {
+      t.type(path: {attribute: 'titleType'})
     }
 
-    t.asset_type(:path => "pbcoreAssetType")
+    t.asset_type(path: "pbcoreAssetType")
 
-    t.broadcast_date(:path => "pbcoreAssetDate", :attributes =>{:dateType=>"broadcast"})
+    t.broadcast_date(path: "pbcoreAssetDate", attributes: {dateType: "broadcast"})
     
-    t.category(:path=>"pbcoreSubject", :attributes=>{:subjectType=>"Category"})
+    t.category(path: "pbcoreSubject", attributes: {subjectType: "Category"})
 
     # Terminology for <pbcoreDescription>
 
-    t.summary(:path => 'pbcoreDescription', :attributes => {:descriptionType => "Summary"})
+    t.summary(path: 'pbcoreDescription', attributes: {descriptionType: "Summary"})
     
     # Creator names and roles
-    t.creator(:path=>"pbcoreCreator") do
+    t.creator(path: "pbcoreCreator") do
       t.creator
-      t.role_(:path=>"creatorRole")
+      t.role_(path: "creatorRole")
     end
-    t.creator_name(:ref=>[:creator, :creator], :type => :string)
-    t.creator_role(:ref=>[:creator, :role], :type => :string)
+    t.creator_name(ref: [:creator, :creator], type: :string)
+    t.creator_role(ref: [:creator, :role], type: :string)
 
-    t.all_descriptions(:path => 'pbcoreDescription') {
-      t.type(:path => {:attribute => 'descriptionType'})
+    t.all_descriptions(path: 'pbcoreDescription') {
+      t.type(path: {attribute: 'descriptionType'})
     }
 
-    t.rights(:path => 'pbcoreRightsSummary/rightsEmbedded/WGBH_RIGHTS') {
-      t.rights(:path => '@RIGHTS')
-      t.coverage(:path => '@RIGHTS_COVERAGE')
-      t.credit(:path => '@RIGHTS_CREDIT')
-      t.holder(:path => '@RIGHTS_HOLDER')
-      t.note(:path => '@RIGHTS_NOTE')
-      t.type(:path => '@RIGHTS_TYPE')
+    t.rights(path: 'pbcoreRightsSummary/rightsEmbedded/WGBH_RIGHTS') {
+      t.rights(path: '@RIGHTS')
+      t.coverage(path: '@RIGHTS_COVERAGE')
+      t.credit(path: '@RIGHTS_CREDIT')
+      t.holder(path: '@RIGHTS_HOLDER')
+      t.note(path: '@RIGHTS_NOTE')
+      t.type(path: '@RIGHTS_TYPE')
     }
     
-    t.instantiations(:path => "pbcoreInstantiation") {
-      t.media_type(:path => 'instantiationMediaType')
+    t.instantiations(path: "pbcoreInstantiation") {
+      t.media_type(path: 'instantiationMediaType')
       t.digital(path: 'instantiationDigital')
-      t.id(:path => 'instantiationIdentifier') {
-        t.source(:path => {:attribute => 'source'})
+      t.id(path: 'instantiationIdentifier') {
+        t.source(path: {attribute: 'source'})
       }
     }
 
     # Relations
-    t.relations(:path => 'pbcoreRelation') {
-      t.type(:path => 'pbcoreRelationType')
-      t.id(:path => 'pbcoreRelationIdentifier')
+    t.relations(path: 'pbcoreRelation') {
+      t.type(path: 'pbcoreRelationType')
+      t.id(path: 'pbcoreRelationIdentifier')
     }
 
     # Coverage
     t.coverage(path: 'pbcoreCoverage') {
-      t.date_portrayed(path: 'coverage', :attributes=>{ ref: "DATE_PORTRAYED" })
+      t.date_portrayed(path: 'coverage', attributes: { ref: "DATE_PORTRAYED" })
       t.type(path: 'coverageType')
     }
 
