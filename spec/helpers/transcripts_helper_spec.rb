151acc50b32cd3b705e685a466a9b5f37f5b6d1f
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-10-09 11:24:59 -0400

    before(:all) was nested too deeply, leading to an order dependency. We were also initializing a lot of stuff that never gets used.

--- spec/helpers/transcripts_helper_spec.rb
+++ spec/helpers/transcripts_helper_spec.rb
@@ -5,19 +5,14 @@ include TranscriptsHelper
 
 describe "render_transcript" do
 
+  before(:all) do
+    Fixtures.cwd("#{fixture_path}/pbcore")
+  end
+  
   describe "for transcripts" do
-    before(:all) do
-      Fixtures.cwd("#{fixture_path}/pbcore")
-      @video = Video.create
-      @video.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/video_3.xml")
-      @transcript = Transcript.new
-      @transcript.pbcore.ng_xml = Fixtures.raw("artesia/patriots_day/transcript_1.xml")
-      @transcript.tei.ng_xml = Fixtures.raw("../tei/Patriots_Day_tei.xml")
-      @video.transcripts << @transcript
-      @video = OpenvaultAsset.find(@video.pid, cast: true)
-    end
-
     it "changes smil to proper html markup" do
+      @transcript = Transcript.new
+      @transcript.tei.ng_xml = Fixtures.raw("../tei/Patriots_Day_tei.xml")
       transformation = render_transcript(@transcript.tei)
       expect(transformation).not_to include ("smil:begin")
       expect(transformation).to include("data-timecodebegin")
