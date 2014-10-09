fd5de7a56c356b062ca915329348ec938ef12394
Andrew Myers <afredmyers@gmail.com>
2014-10-09 15:56:50 -0400

    Moves helper methods specific to CustomCollectionItem out into it's own module.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,21 +9,4 @@ module ApplicationHelper
     content_for(:title, page_title.to_s)
     return content_tag(:h1, page_title, options)
   end
-
-  def collection_item_link(item, options={})
-  	id = item.solr_doc['id']
-  	title = "#{item.solr_doc['title_tesim'].first} - #{item.kind}"
-  	path = catalog_path(id)
-  	link_to title, path, options
-  end
-
-  def collection_item_path(item)
-    item.kind.downcase
-  end
-
-  # TODO: Is it OK to have Fedora lookup in this helper method?
-  def render_collection_item(item, options={})
-    partial_name = (item.kind == "Video") ? 'video' : 'other'
-    render partial: "/custom_collections/#{partial_name}", locals: {item: item, ov_asset: item.ov_asset}
-  end
 end
