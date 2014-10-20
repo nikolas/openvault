eab1089392133bb97be3b733f4a2c19e88096db3
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-10-20 14:22:02 -0400

    Unrecognized urls should just be 404, not 500.

--- app/controllers/override_controller.rb
+++ app/controllers/override_controller.rb
@@ -1,13 +1,19 @@
 class OverrideController < ApplicationController  
   
   def show
-    raise 'Bad URL' unless params[:path] =~ /^[a-z\/-]+$/ # should have been caught by routes.rb
+    render_404 unless params[:path] =~ /^[a-z0-9\/-]+$/i # paranoid about weird paths.
     override_file_path = "override/#{params[:path]}.html.erb"
     if File.exists?("app/views/#{override_file_path}")
       render file: override_file_path
     else
-      render 'catalog/no_record_found', status: :not_found, formats: :html
+      render_404
     end
+  end
+  
+  private
+  
+  def render_404
+    render 'catalog/no_record_found', status: :not_found, formats: :html
   end
 
 end 
