305bcea6564eba4b1b1e81b90e50c14dda3aec0b
Andrew Myers <afredmyers@gmail.com>
2014-11-10 13:52:19 -0500

    Adds a capistrano task for setting up Google Analytics account ID in a Rails initializer. Adds a config/deploy/recipes/ dir, for this task, and task for basic http auth, in an effort to keep deploy.rb clean.

---
+++ config/deploy/recipes/basic_http_auth.rb
@@ -0,0 +1,46 @@
+# Link the latest release to any existing basic http auth initializer
+after 'deploy:update_code', 'link_shared:initializer:basic_http_auth'
+
+
+namespace :deploy do
+  desc 'Set basic http authentication'
+  task :set_http_basic_auth, roles: :app do
+    puts "Setting up basic http authentication..."
+    set(:username, Capistrano::CLI.ui.ask("Enter a username: "))
+    set(:http_password, Capistrano::CLI.password_prompt("Enter a password: "))
+
+    username_constraint = /^[A-Za-z0-9 _\-!@$%^&*+=\(\)\[\]:;"<>,\.\?\/#]{1,20}$/
+    password_constraint = /^[A-Za-z0-9 _\-!@$%^&*+=\(\)\[\]:;"<>,\.\?\/#]{6,20}$/
+
+    raise "Username must match this regex: #{username_constraint.inspect}" unless username =~ username_constraint
+    raise "Password must match this regex: #{password_constraint.inspect}" unless password =~ password_constraint
+
+    file_contents = <<-EOS
+# Environment varaibles to be use for basic HTTP authentication.
+ENV['HTTP_BASIC_AUTH_USER'] = '#{username}'
+ENV['HTTP_BASIC_AUTH_PASSWORD'] = '#{http_password}'
+EOS
+
+    put file_contents, "#{shared_path}/config/initializers/basic_http_auth.rb"
+    link_shared.initializer.basic_http_auth
+    restart
+  end
+
+  desc 'Remove basic http authentication'
+  task :remove_http_basic_auth, roles: :app do
+    # remove the symlink
+    run "rm #{latest_release}/config/initializers/basic_http_auth.rb"
+
+    # remove the shared file
+    run "rm #{shared_path}/config/initializers/basic_http_auth.rb"
+  end
+end
+
+namespace :link_shared do
+  namespace :initializer do
+    desc "Link to shared initializer for basic http auth"
+    task :basic_http_auth do
+      run "test -f #{shared_path}/config/initializers/basic_http_auth.rb && ln -nfs #{shared_path}/config/initializers/basic_http_auth.rb #{latest_release}/config/initializers/basic_http_auth.rb"
+    end
+  end
+end
---
+++ config/deploy/recipes/google_analytics.rb
@@ -0,0 +1,26 @@
+# Link the latest release to any existing google analytics initializer
+after 'deploy:update_code', 'link_shared:initializer:google_analytics'
+
+namespace :deploy do
+  desc 'Set Google Analytics ID'
+  task :set_ga_id, roles: :app do
+    set(:ga_id, Capistrano::CLI.ui.ask("Enter the Google Analytics account ID: "))
+
+    file_contents = <<-EOS
+# Google Analytics account ID
+ENV['GA_TRACKING_ID'] = '#{ga_id}'
+EOS
+
+    put file_contents, "#{shared_path}/config/initializers/google_analytics.rb"
+    link_shared.initializer.google_analytics
+    restart
+  end
+end
+
+namespace :link_shared do
+  namespace :initializer do
+    task :google_analytics do
+      run "test -f #{shared_path}/config/initializers/google_analytics.rb && ln -nfs #{shared_path}/config/initializers/google_analytics.rb #{latest_release}/config/initializers/google_analytics.rb"
+    end
+  end
+end
