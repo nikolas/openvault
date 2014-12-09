f83434c6e22272951fa0df9c665e4a68efbee16b
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-12-09 12:48:48 -0500

    test ListIdentifiers

--- spec/controllers/oai_controller_spec.rb
+++ spec/controllers/oai_controller_spec.rb
@@ -18,6 +18,7 @@ describe OaiController do
   oai_verb_works('ListMetadataFormats')
   oai_verb_works('ListSets')
   oai_verb_works('GetRecord', identifier: '123', metadataPrefix: 'pbcore')
+  oai_verb_works('ListIdentifiers', metadataPrefix: 'pbcore')
   oai_verb_works('ListRecords', metadataPrefix: 'pbcore') { |test, xml|
     test.expect(xml).to test.match /<record><header><identifier>/
   }
