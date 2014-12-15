b6afca370128935bd54d6413902ffcb177a704e5
Chuck McCallum <chuck_mccallum@wgbh.org>
2014-12-15 13:39:13 -0500

    Failing test.

--- spec/routing/blog_redirect_spec.rb
+++ spec/routing/blog_redirect_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe '/blog', type: :request do
   it 'redirects to blog.openvault.wgbh.org' do
-    path = '/foo'
+    path = '/foo/bar'
     get "/blog#{path}"
     expect(response).to redirect_to("http://blog.openvault.wgbh.org#{path}")
   end
