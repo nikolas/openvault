a0cecdabedc745070d7e7f8fc80ad8c425cd5b7d
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-01-06 17:20:36 -0500

    Purge unused lines from pbcore_desc_doc. ("summary" is just one.)

--- app/models/datastream/pbcore_desc_doc.rb
+++ app/models/datastream/pbcore_desc_doc.rb
@@ -15,25 +15,9 @@ class PbcoreDescDoc < ActiveFedora::OmDatastream
 
     t.program_title(path: "pbcoreTitle", attributes: {titleType: "Program"})
 
-    t.chapter_title(path: "pbcoreTitle", attributes: { titleType: "Chapter" })
-
     t.episode_title(path: "pbcoreTitle", attributes: { titleType: "Episode" })
-    
-    t.element_title(path: "pbcoreTitle", attributes: { titleType: "Element" })
-    
-    t.clip_title(path: "pbcoreTitle", attributes: { titleType: "Clip" })
-    
-    t.label(path: "pbcoreTitle", attributes: { titleType: "Label" })
-    
-    t.segment_title(path: "pbcoreTitle", attributes: { titleType: "Segment" })
-    
-    t.subtitle(path: "pbcoreTitle", attributes: { titleType: "Subtitle" })
-    
-    t.track_title(path: "pbcoreTitle", attributes: { titleType: "Track" })
 
     t.image_title(path: "pbcoreTitle", attributes: { titleType: "Image" })
-
-    t.translation_title(path: "pbcoreTitle", attributes: { titleType: "Translation" })
 
     # Term for inspecting all the titles present in the XML.
     t.all_titles(path: "pbcoreTitle") {
@@ -43,12 +27,6 @@ class PbcoreDescDoc < ActiveFedora::OmDatastream
     t.asset_type(path: "pbcoreAssetType")
 
     t.broadcast_date(path: "pbcoreAssetDate", attributes: {dateType: "broadcast"})
-    
-    t.category(path: "pbcoreSubject", attributes: {subjectType: "Category"})
-
-    # Terminology for <pbcoreDescription>
-
-    t.summary(path: 'pbcoreDescription', attributes: {descriptionType: "Summary"})
     
     # Creator names and roles
     t.creator(path: "pbcoreCreator") do
