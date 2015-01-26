08860c5c491a35d698aca911825dc1fe115fbd9f
Andrew Myers <afredmyers@gmail.com>
2015-01-25 22:08:53 -0500

    Makes 'master' the default branch for both 'prodution' and 'staging' stages.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
 # General info
 set :deploy_to, "/wgbh/http/openvault"
-
-# Github
-set :branch, fetch(:branch, "master")
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,2 @@
 # General info
 set :deploy_to, "/wgbh/http/openvault-staging"
-
-# Github
-set :branch, fetch(:branch, "development")
